1830da98b18ae2ea296e028b9d4e0073920ca2b2
newler55 <newler55@gmail.com>
2019-12-02 20:21:00 +0700

    add blackend setup

---
+++ blackend/src/main/java/se/g21/blackend/BlackendApplication.java
@@ -0,0 +1,13 @@
+package se.g21.blackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackendApplication.class, args);
+	}
+
+}
---
+++ blackend/src/test/java/se/g21/blackend/BlackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.g21.blackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
