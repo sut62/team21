747bb503f80e2c12d9b372ccf85ea6a4fcdac887
newler55 <newler55@gmail.com>
2019-12-03 01:51:51 +0700

    add file all

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,75 @@
 <template>
   <v-app>
+    <v-app-bar app >
+      <v-toolbar-title class="headline text-uppercase">
+        <span>System</span>
+        <span class="font-weight-light">SE-G21</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="Home">
+        <span class="mr-2">HOME</span>
+      </v-btn>
+      <v-btn @click="Employee">
+        <span class="mr-2">Employee</span>
+      </v-btn>
+      <v-btn @click="Student">
+        <span class="mr-2">Student</span>
+      </v-btn>
+      <v-btn @click="Course">
+        <span class="mr-2">Course</span>
+      </v-btn>
+      <v-btn @click="CourseRegistration">
+        <span class="mr-2">CourseRegistration</span>
+      </v-btn>
+      <v-btn @click="Expenses">
+        <span class="mr-2">ExpensesManagement</span>
+      </v-btn>
+      <v-btn @click="Reviews">
+        <span class="mr-2">Reviews</span>
+      </v-btn>
+    </v-app-bar>
+
     <v-content>
       <router-view />
     </v-content>
+
   </v-app>
 </template>
+<script>
+export default {
+  data: () => ({
+    icons: [
+      "fab fa-facebook",
+      "fab fa-twitter",
+      "fab fa-google-plus",
+      "fab fa-linkedin",
+      "fab fa-instagram"
+    ]
+  }),
+  methods: {
+    Home() {
+      this.$router.push("/");
+    },
+    Employee() {
+      this.$router.push("/AddEmployee");
+    },
+    Student() {
+      this.$router.push("/AddStudent");
+    },
+    Course() {
+      this.$router.push("/AddCourse");
+    },
+    CourseRegistration() {
+      this.$router.push("/CourseRegister");
+    },
+    Expenses() {
+      this.$router.push("/StudentExpenses");
+    },
+    Reviews() {
+      this.$router.push("/AddReviews");
+    }
+  }
+};
+</script>
+
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router';
 
-// import Home from '../components/HomePages/Home';
+import Home from '../components/HomePages/Home';
+import AddEmployee from '../components/EmployeeManagementSystem/AddEmployee';
+import AddStudent from '../components/StudentManagemenSystem/AddStudent';
+import AddCourse from '../components/CourseManagementSystem/AddCourse';
 import CourseRegister from '../components/CourseRegistrationManagementSystem/CourseRegister';
+import StudentExpenses from '../components/ExpensesManagementSystem/StudentExpenses';
+import AddReviews from '../components/ReviewsManagementSystem/AddReviews';
 
 Vue.use(Router);
 
@@ -11,7 +16,31 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [{
             path: '/',
+            component: Home
+        },
+        {
+            path: '/AddEmployee',
+            component: AddEmployee
+        },
+        {
+            path: '/AddStudent',
+            component: AddStudent
+        },
+        {
+            path: '/AddCourse',
+            component: AddCourse
+        },
+        {
+            path: '/CourseRegister',
             component: CourseRegister
+        },
+        {
+            path: '/StudentExpenses',
+            component: StudentExpenses
+        },
+        {
+            path: '/AddReviews',
+            component: AddReviews
         },
 
     ]
