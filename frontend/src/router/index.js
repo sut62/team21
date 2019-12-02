b04d2b4fda0d9259c83f1267b4a8f22bcb6538c4
newler55 <newler55@gmail.com>
2019-12-03 00:51:15 +0700

    add file all

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router';
 
-import Home from '../components/Home';
+// import Home from '../components/HomePages/Home';
+import CourseRegister from '../components/CourseRegistrationManagementSystem/CourseRegister';
 
 Vue.use(Router);
 
@@ -10,8 +11,8 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [{
             path: '/',
-            component: Home
+            component: CourseRegister
         },
-        
+
     ]
 });
