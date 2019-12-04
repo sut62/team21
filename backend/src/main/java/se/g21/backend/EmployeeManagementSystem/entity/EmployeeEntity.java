34a209e86221eea8c3314bb9daa7c2b43ecd10a1
pongsakorn-maker <pongsakorn.reo@gmail.com>
2019-12-04 18:32:40 +0700

    close #38 - ทำ Entity ของระบบจัดเก็บข้อมูลพนักงาน

--- backend/src/main/java/se/g21/backend/EmployeeManagementSystem/entity/EmployeeEntity.java
+++ backend/src/main/java/se/g21/backend/EmployeeManagementSystem/entity/EmployeeEntity.java
@@ -1,1 +1,34 @@
 package se.g21.backend.EmployeeManagementSystem.entity;
+
+import lombok.*;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.Collection;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "Employee")
+public class EmployeeEntity {
+	@Id
+	@SequenceGenerator(name = "Employee_seq", sequenceName = "Employee_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Employee_seq")
+	@Column(name = "Employee_ID", unique = true, nullable = true)
+    private @NonNull Long id;
+    
+	@Column(name = "Title_ID", nullable = true)
+    private @NonNull Title title;
+    
+    private @NonNull String firstname;
+    
+	private @NonNull String lastname;
+    @Column(name = "Gender_ID", nullable = true)
+	private @NonNull Gender gender;
+}
