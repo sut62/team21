43ef58504e06559071048193d3cd993c4a90eadc
bluesun1 <diveman2011@gmail.com>
2019-12-09 15:11:51 +0700

    add other entity in StudentManagement

--- backend/src/main/java/se/g21/backend/StudentManagemenSystem/entity/StudentEntity.java
+++ backend/src/main/java/se/g21/backend/StudentManagemenSystem/entity/StudentEntity.java
@@ -1,1 +1,56 @@
 package se.g21.backend.StudentManagemenSystem.entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="Student")
+public class StudentEntity {
+
+    @Id
+    @SequenceGenerator(name="student_seq",sequenceName="student_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="student_seq")
+    @Column(name = "STUDENT_ID", unique = true, nullable = true)
+    private @NonNull Long id;
+    private @NonNull String name;
+    private @NonNull Long old;
+    @Column(name="RENT_DATE")
+    private @NonNull Date rentDate;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Gender.class)
+    @JoinColumn(name = "GENDER_ID", insertable = true)
+    private Gender gender;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Province.class)
+    @JoinColumn(name = "PROVINCE_ID", insertable = true)
+    private Province province;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Titlename.class)
+    @JoinColumn(name = "TITLENAME_ID", insertable = true)
+    private Titlename titlename;
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setOld(Long old){
+        this.old=old;
+    }
+    
+}
