541bda7bcf5be59309958c6722c7cb2d826de1a4
pongsakorn-maker <pongsakorn.reo@gmail.com>
2019-12-29 20:02:29 +0700

    add repository employeesystem - close #46

--- backend/src/main/java/se/g21/backend/employeesystem/repository/GenderRepository.java
+++ backend/src/main/java/se/g21/backend/employeesystem/repository/GenderRepository.java
@@ -1,9 +1,10 @@
 package se.g21.backend.employeesystem.repository;
 
-import se.g21.backend.employeesystem.entities.Employee;
+import se.g21.backend.employeesystem.entities.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface GenderRepository extends JpaRepository<Gender, Long> {
+    Gender findById(long id);
 }
--- backend/src/main/java/se/g21/backend/employeesystem/repository/NametitleRepository.java
+++ backend/src/main/java/se/g21/backend/employeesystem/repository/NametitleRepository.java
@@ -1,9 +1,11 @@
 package se.g21.backend.employeesystem.repository;
 
-import se.g21.backend.employeesystem.entities.Employee;
+import se.g21.backend.employeesystem.entities.Nametitle;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+public interface NametitleRepository extends JpaRepository<Nametitle, Long> {
+    public Nametitle findById(long employee);
 }
