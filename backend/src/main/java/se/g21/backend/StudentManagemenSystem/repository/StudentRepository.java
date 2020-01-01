b2b730d8578cebbaaf87184b93b78d49fc2f7aae
bluesun1 <diveman2011@gmail.com>
2020-01-01 23:28:14 +0700

    close #49 - add student repo

--- backend/src/main/java/se/g21/backend/StudentManagemenSystem/repository/StudentRepository.java
+++ backend/src/main/java/se/g21/backend/StudentManagemenSystem/repository/StudentRepository.java
@@ -1,1 +1,10 @@
 package se.g21.backend.StudentManagemenSystem.repository;
+
+import se.g21.backend.StudentManagemenSystem.entity.StudentEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
+}
