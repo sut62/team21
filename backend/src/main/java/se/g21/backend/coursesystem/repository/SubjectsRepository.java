5b1a628e7e41ef33f96bb91e3091519f9eef09f9
keangkai18342 <Higgykaii@gmail.com>
2020-01-04 19:29:34 +0700

    close #59 add repository coursesystem

--- backend/src/main/java/se/g21/backend/coursesystem/repository/SubjectsRepository.java
+++ backend/src/main/java/se/g21/backend/coursesystem/repository/SubjectsRepository.java
@@ -1,10 +1,10 @@
 package se.g21.backend.coursesystem.repository;
 
-import se.g21.backend.coursesystem.entities.Course;
+import se.g21.backend.coursesystem.entities.Subjects;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CourseRepository extends JpaRepository<Course, Long> {
-    Course findById(long id);
+public interface SubjectsRepository extends JpaRepository<Subjects, Long> {
+    Subjects findById(long id);
 }
