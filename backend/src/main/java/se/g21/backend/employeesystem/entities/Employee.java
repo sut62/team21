633db2eefda59156aa494192add02090ead247e0
pongsakorn-maker <pongsakorn.reo@gmail.com>
2020-01-22 18:42:26 +0700

    close #127 - ทำ แก้บัค TestCase บางส่วนของระบบจัดเก็บข้อมูลพนักงาน

--- backend/src/main/java/se/g21/backend/employeesystem/entities/Employee.java
+++ backend/src/main/java/se/g21/backend/employeesystem/entities/Employee.java
@@ -9,6 +9,7 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -78,7 +79,6 @@ public class Employee {
 	private Date recorddate;
 
 	@Email
-	@NotNull
 	@Column(name = "Email")
 	private String email;
 
