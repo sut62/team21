75407bb71104c145b426eec4578b6a9c2033a9d0
newler55 <newler55@gmail.com>
2020-01-22 19:02:34 +0700

    update entity EnrollCourse and Computer and dataLoader

--- backend/src/main/java/se/g21/backend/enrollcoursesystem/entities/Computer.java
+++ backend/src/main/java/se/g21/backend/enrollcoursesystem/entities/Computer.java
@@ -3,6 +3,8 @@ package se.g21.backend.enrollcoursesystem.entities;
 import lombok.*;
 import lombok.NonNull;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import se.g21.backend.coursesystem.entities.Room;
 
@@ -20,15 +22,16 @@ public class Computer {
     @Column(name="Computer_ID",unique = true, nullable = true)
     private @NonNull Long id;
 
-    private @NonNull String pcNumber;
+    @Pattern(regexp = "[P][C][0-9][0-9]")
+    @NotNull
+    private String pcNumber;
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Room.class)
     @JoinColumn(name = "Room_ID", insertable = true)
+    @NotNull
     private Room room;
 
     @OneToMany(fetch = FetchType.LAZY)
     private Collection<EnrollCourse> enrollCourse;
-
-    
 
 }
