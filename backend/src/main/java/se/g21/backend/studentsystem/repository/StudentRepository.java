f61f09064500dccc04bc600d6ea9455ad1a56496
bluesun1 <diveman2011@gmail.com>
2020-01-24 22:30:47 +0700

    close #146 - edit student repository

--- backend/src/main/java/se/g21/backend/studentsystem/repository/StudentRepository.java
+++ backend/src/main/java/se/g21/backend/studentsystem/repository/StudentRepository.java
@@ -1,11 +1,16 @@
 package se.g21.backend.studentsystem.repository;
 
 import se.g21.backend.studentsystem.entities.Student;
-
+import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Student findById(long id);
+
+    @Query(value = "SELECT * FROM STUDENT WHERE FULLNAME LIKE :fullname ", nativeQuery = true)
+    Collection<Student> findByFullname(@Param("fullname") String fullname);
 }
